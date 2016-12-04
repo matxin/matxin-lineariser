4809672bf9b41d7cda7173e0eadd66252c45f47c
matthew <m5w@users.noreply.github.com>
2016-12-04 16:23:25 -0500

    Start prototyping second paper with CONLL-U word lines; Ignore swp and pyc files

---
+++ C12-2127/wordline.py
@@ -0,0 +1,13 @@
+class WordLine:
+    __init__(self, id_, form, lemma, upostag, xpostag, feats, head, deprel, \
+             deps, misc):
+        self.id_ = id_
+        self.form = form
+        self.lemma
+        self.upostag = upostag
+        self.xpostag = xpostag
+        self.feats = feats
+        self.head = head
+        self.deprel = deprel
+        self.deps = deps
+        self.misc = misc
