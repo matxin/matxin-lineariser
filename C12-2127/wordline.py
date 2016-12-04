8a429a083e50ffd4822229e6b6be21b5d19ff3cb
matthew <m5w@users.noreply.github.com>
2016-12-04 17:07:05 -0500

    Make WordLine ctor read from a line

--- C12-2127/wordline.py
+++ C12-2127/wordline.py
@@ -1,13 +1,39 @@
 class WordLine:
-    __init__(self, id_, form, lemma, upostag, xpostag, feats, head, deprel, \
-             deps, misc):
-        self.id_ = id_
-        self.form = form
-        self.lemma
-        self.upostag = upostag
-        self.xpostag = xpostag
-        self.feats = feats
-        self.head = head
-        self.deprel = deprel
-        self.deps = deps
-        self.misc = misc
+    def __init__(self, line):
+        fields = line.split('\t')
+        # parse the fields in reverse order by popping them off the end
+        misc = fields.pop()
+
+        if misc == '_':
+            self.misc = None
+        else:
+            self.misc = dict(field.split('=') for field in misc.split('|'))
+
+        deps = fields.pop()
+
+        if deps == '_':
+            self.deps = None
+        else:
+            self.deps = dict(dep.split(':') for dep in deps.split('|'))
+
+        self.deprel = fields.pop()
+        self.head = int(fields.pop())
+        feats = fields.pop()
+
+        if feats == '_':
+            self.feats = None
+        else:
+            self.feats = dict(feat.split('=') for feat in feats.split('|'))
+
+        xpostag = fields.pop()
+
+        if xpostag == '_':
+            self.xpostag = None
+        else:
+            self.xpostag = xpostag
+
+        self.upostag = fields.pop()
+        self.lemma = fields.pop()
+        self.form = fields.pop()
+        self.id_ = int(fields.pop()) # to-do: support multiword integer ranges
+                                     # e.g. 1-2 or 3.5
