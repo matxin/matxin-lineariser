f74035e655a10b60305a2668fdad204c91020663
matthew <m5w@users.noreply.github.com>
2016-12-09 01:31:29 -0500

    Automate printing

--- utlgrammars/test/hypothesise_node.py
+++ utlgrammars/test/hypothesise_node.py
@@ -3,6 +3,7 @@ import sys
 sys.path.append('..')
 
 from lineariser import Lineariser
+from printing import Printing
 from sentence import Sentence
 
 with open('hypothesise_node.conllu') as conllu:
@@ -14,3 +15,4 @@ with open('hypothesise_node.xml') as xml:
     lineariser.deserialise(xml)
 
 hypotheses = [lineariser.hypothesise_node(treebank[0].get_root(), 0)]
+print(Printing.print_list(hypotheses))
