3cc013e4b41ca2caa3e3057321cf89d27f62f09b
matthew <m5w@users.noreply.github.com>
2016-12-08 22:21:22 -0500

    Add list printing

--- utlgrammars/printing.py
+++ utlgrammars/printing.py
@@ -4,27 +4,37 @@ class Printing:
         return str_.replace('\n', '\n' + ' ' * shift * shiftwidth)
 
     @classmethod
-    def print_str(cls, str_):
-        return 'str of len ' + str(len(str_)) + ' = ' + repr(str_)
-
-    @classmethod
     def print_dict(cls, dict_, print_key=repr, print_value=repr):
         def print_item(item):
             return print_key(item[0]) + ': ' + print_value(item[1])
 
         dict_items_list_ = list(dict_.items())
         dict_items_list_.sort()
-        dict_len_ = len(dict_)
-        str_ = 'dict of len ' + str(dict_len_) + ' = {'
+        return cls.print_iterable(
+            cls.get_name(dict_), dict_items_list_, print_item)
 
-        if dict_len_ != 0:
-            str_ += '\n  ' + print_item(dict_items_list_[0])
+    @classmethod
+    def get_name(cls, object_):
+        class_ = object_.__class__
+        return class_.__module__ + '.' + class_.__name__
 
-            for index in range(1, dict_len_):
+    @classmethod
+    def print_iterable(cls, name_, iterable_, print_item):
+        iterable_len_ = len(iterable_)
+        str_ = name_ + ' of len ' + str(iterable_len_) + ' = {'
+
+        if iterable_len_ != 0:
+            str_ += '\n  ' + print_item(iterable_[0])
+
+            for index in range(1, iterable_len_):
                 str_ += ',\n' + \
-                        '  ' + print_item(dict_items_list_[index])
+                        '  ' + print_item(iterable_[index])
 
             str_ += '\n'
 
         str_ += '}'
         return str_
+
+    @classmethod
+    def print_list(cls, list_, print_item=repr):
+        return cls.print_iterable(cls.get_name(list_), list_, print_item)
