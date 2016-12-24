b763fa842d107dd0050cc6cf63c6b1ac04b90a70
matthew <m5w@users.noreply.github.com>
2016-12-24 16:00:19 -0500

    Print hypotheses and references out to file and add scoring script

--- utlgrammars/bleu.py
+++ utlgrammars/bleu.py
@@ -4,37 +4,32 @@ from sentence import Sentence
 import nltk
 
 from argparse import ArgumentParser
-from sys import stdin
+from sys import stdin, stdout, stderr
 import traceback
 
-try:
-    argument_parser = ArgumentParser()
-    argument_parser.add_argument('xml')
-    arguments = argument_parser.parse_args()
-    lineariser = Lineariser()
+argument_parser = ArgumentParser()
+argument_parser.add_argument('xml')
+arguments = argument_parser.parse_args()
+lineariser = Lineariser()
 
-    with open(arguments.xml) as xml:
-        lineariser.deserialise(xml)
+with open(arguments.xml) as xml:
+    lineariser.deserialise(xml)
 
-    references = []
-    hypotheses = []
+references = []
+hypotheses = []
 
-    iter_ = (sentence for sentence in Sentence.deserialise(stdin))
+for sentence in Sentence.deserialise(stdin):
+    reference = list(sentence.get_sentence().items())
+    reference.sort()
+    reference = ' '.join([value.get_form().lower() for key, value in reference])
+    print(reference)
+    stdout.flush()
+    sentence.linearise(lineariser, 1)
+    hypothesis = sentence.get_strings()[0]
+    print(hypothesis, file=stderr)
+    stderr.flush()
 
-    while True:
-        try:
-            sentence = next(iter_)
-        except (StopIteration):
-            break
-        except (AttributeError):
-            traceback.print_exc()
-
-        reference = list(sentence.get_sentence().items())
-        reference.sort()
-        references.append([value.get_form().lower() for key, value in reference])
-        sentence.linearise(lineariser, 1)
-        hypotheses.extend(sentence.get_strings())
-
-    print(nltk.translate.bleu_score.corpus_bleu(references, hypotheses))
-except (AttributeError):
-    traceback.print_exc()
+    if len(reference) != len(hypothesis):
+        print('reference = ' + repr(reference))
+        print('hypothesis = ' + repr(hypothesis))
+        sys.exit()
---
+++ utlgrammars/bleu_score.py
@@ -0,0 +1,14 @@
+import nltk
+
+from argparse import ArgumentParser
+
+argument_parser = ArgumentParser()
+argument_parser.add_argument('references')
+argument_parser.add_argument('hypotheses')
+arguments = argument_parser.parse_args()
+
+with open(arguments.references) as references, \
+     open(arguments.hypotheses) as hypotheses:
+    print(nltk.translate.bleu_score.corpus_bleu([
+        line[:-1].split(' ') for line in references
+    ], [line[:-1].split(' ') for line in hypotheses]))
