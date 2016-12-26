10a66c548971d8c7a4ddea345eaaf02920c0a413
matthew <m5w@users.noreply.github.com>
2016-12-25 21:41:20 -0500

    Fixes

--- utlgrammars/utlgevaluate.py
+++ utlgrammars/utlgevaluate.py
@@ -21,6 +21,10 @@ with open(arguments.xml) as xml:
 treebank = [sentence for sentence in Sentence.deserialise(stdin)]
 
 
+def default_reference_function(reference):
+    pass
+
+
 def sample(reference_function=default_reference_function):
     bleu_scores = []
 
@@ -28,6 +32,7 @@ def sample(reference_function=default_reference_function):
         sentence_list = list(sentence.get_sentence().items())
         sentence_list.sort()
         reference = [word.get_form().lower() for _, word in sentence_list]
+        reference_function(reference)
         references = [reference]
         sentence.linearise(lineariser, 1, True)
         linearisation = sentence.get_linearisations()[0]
@@ -41,10 +46,6 @@ def sample(reference_function=default_reference_function):
     return bleu_scores
 
 
-def default_reference_function(reference):
-    pass
-
-
 reference_len_list = []
 
 
@@ -53,47 +54,54 @@ def get_len_(reference):
 
 
 bleu_scores = sample(reference_function=get_len_)
+print()
 print('coverage = ' + str(hypothesis.coverage.get_coverage()))
 
 
 def print_statistics(list_):
     min_ = min(list_)
-    print('               minimum = ' + format_statistic(min_))
+    print('minimum = ' + format_statistic(min_))
     max_ = max(list_)
-    print('               maximum = ' + format_statistic(max_))
-    print('                 range = ' + format_statistic(max_ - min_))
-    print('                  mean = ' + format_statistic(numpy.mean(list_)))
-    print('    standard deviation = ' + format_statistic(numpy.std(list_)))
-    print('                median = ' + format_statistic(numpy.median(list_)))
+    print('maximum = ' + format_statistic(max_))
+    print('range = ' + format_statistic(max_ - min_))
+    print('mean = ' + format_statistic(numpy.mean(list_)))
+    print('standard deviation = ' + format_statistic(numpy.std(list_)))
+    print('median = ' + format_statistic(numpy.median(list_)))
     q_1 = numpy.percentile(list_, 25)
-    print('        first quartile = ' + format_statistic(q_1))
+    print('first quartile = ' + format_statistic(q_1))
     q_3 = numpy.percentile(list_, 75)
-    print('        third quartile = ' + format_statistic(q_3))
-    print('  inter-quartile range = ' + format_statistic(q_3 - q_1))
+    print('third quartile = ' + format_statistic(q_3))
+    print('inter-quartile range = ' + format_statistic(q_3 - q_1))
 
 
 def format_statistic(statistic):
     return '{0:.4f}'.format(statistic)
 
 
-print('sentence lengths:')
+print()
+print('sentence lengths')
+print('================')
 print_statistics(reference_len_list)
 pyplot.figure()
-pyplot.hist(reference_len_list, bins=5, range=(0, 50))
+pyplot.hist(reference_len_list, bins=10, range=(0, 50))
 pyplot.title('Reference Length Frequency Histogram')
 pyplot.xlabel('Reference Length (# of Words)')
 pyplot.ylabel('# of References')
-print('bleu scores:')
+print()
+print('bleu scores')
+print('===========')
 print_statistics(bleu_scores)
 pyplot.figure()
-pyplot.hist(bleu_scores, bins=5, range=(0.5, 1.0))
+pyplot.hist(bleu_scores, bins=10, range=(0.5, 1.0))
 pyplot.title('BLEU Score Frequency Histogram')
 pyplot.xlabel('BLEU Score')
 pyplot.ylabel('# of References')
 mean_bleu_scores = [numpy.mean(bleu_scores)]
 mean_bleu_scores.extend(
-    [numpy.mean(sample()) for _ in range(
-        arguments.samples, start=1)])
-print('mean bleu scores:')
+    [numpy.mean(sample()) for _ in range(1, arguments.samples)])
+print()
+print('mean bleu scores')
+print('================')
 print_statistics(mean_bleu_scores)
+print()
 pyplot.show()
