c721d3801f22dea7d3d63855a9836ace9a1c23a4
matthew <m5w@users.noreply.github.com>
2017-01-08 17:29:35 -0500

    Evaluate using moses-smt's mteval

--- utlgevaluate.py
+++ utlgevaluate.py
@@ -8,63 +8,43 @@ import numpy
 
 from argparse import ArgumentParser
 from sys import stdin, stdout, stderr
+import re
+import subprocess
 
 argument_parser = ArgumentParser()
 argument_parser.add_argument('xml')
 argument_parser.add_argument('samples', type=int)
+argument_parser.add_argument('mteval')
+argument_parser.add_argument('ref_file')
+argument_parser.add_argument('src_file')
+argument_parser.add_argument('tst_file')
 arguments = argument_parser.parse_args()
 lineariser = Lineariser()
 
 with open(arguments.xml) as xml:
     lineariser.deserialise(xml)
 
+reference_len_list = []
 treebank = [sentence for sentence in Sentence.deserialise(stdin)]
 
 
-def default_reference_function(reference):
-    pass
+def print_seg(id_, seg, file):
+    print('<seg id="' + str(id_) + '">' + ' '.join(seg) + '</seg>', file=file)
 
 
-def get_bleu_score_sample(reference_function=default_reference_function):
-    bleu_scores = []
-
-    for sentence in treebank:
+with open(arguments.ref_file, mode='w') as ref_file, \
+     open(arguments.src_file, mode='w') as src_file:
+    for id_, sentence in enumerate(treebank, start=1):
         sentence_list = list(sentence.get_wordlines().items())
         sentence_list.sort()
         reference = [word.get_form().lower() for _, word in sentence_list]
-        reference_function(reference)
-        references = [reference]
-        sentence.linearise(lineariser, 1, True)
-        linearisation = sentence.get_linearisations()[0]
-        hypothesis = [word.get_form().lower() for word in linearisation]
-        hypothesis_len = len(hypothesis)
-        if hypothesis_len < 4:
-            weights = (1.0 / hypothesis_len, ) * hypothesis_len
-        else:
-            weights = (0.25,) * 4
-        bleu_scores.append(
-            bleu_score.sentence_bleu(
-                references, hypothesis, weights=weights))
-
-    return bleu_scores
-
-
-reference_len_list = []
-
-
-def get_len_(reference):
-    reference_len_list.append(len(reference))
-
-
-bleu_scores = get_bleu_score_sample(reference_function=get_len_)
-print()
+        reference_len_list.append(len(reference))
+        print_seg(id_, reference, ref_file)
+        print_seg(id_, reference, src_file)
 
 
 def format_statistic(statistic):
     return '{0:.4f}'.format(statistic)
-
-
-print('coverage = ' + format_statistic(hypothesis.coverage.get_coverage()))
 
 
 def print_statistics(list_):
@@ -74,14 +54,14 @@ def print_statistics(list_):
     max_ = max(list_)
     print('maximum = ' + format_statistic(max_))
     print('range = ' + format_statistic(max_ - min_))
-    print('mean = ' + format_statistic(numpy.mean(list_)))
-    print('standard deviation = ' + format_statistic(numpy.std(list_)))
     print('median = ' + format_statistic(numpy.median(list_)))
     q_1 = numpy.percentile(list_, 25)
     print('first quartile = ' + format_statistic(q_1))
     q_3 = numpy.percentile(list_, 75)
     print('third quartile = ' + format_statistic(q_3))
     print('inter-quartile range = ' + format_statistic(q_3 - q_1))
+    print('mean = ' + format_statistic(numpy.mean(list_)))
+    print('standard deviation = ' + format_statistic(numpy.std(list_)))
 
 
 print()
@@ -93,16 +73,29 @@ pyplot.hist(reference_len_list, bins=10, range=(0, 50))
 pyplot.title('Reference Length Frequency Histogram')
 pyplot.xlabel('Reference Length (# of Words)')
 pyplot.ylabel('# of References')
+BLEU_SCORE = re.compile('BLEU score = ([01]\.\d{4,4})')
+
+
+def get_sample_bleu_score():
+    with open(arguments.tst_file, mode='w') as tst_file:
+        for id_, sentence in enumerate(treebank, start=1):
+            sentence.linearise(lineariser, 1, True)
+            linearisation = sentence.get_linearisations()[0]
+            hypothesis = [word.get_form().lower() for word in linearisation]
+            print_seg(id_, hypothesis, tst_file)
+
+    completed_process = subprocess.run([
+        'perl', arguments.mteval, '-r', arguments.ref_file, 's',
+        arguments.src_file, 't', arguments.tst_file
+    ],
+                                       stdout=subprocess.PIPE)
+    completed_process.check_returncode()
+    return float(BLEU_SCORE.search(completed_process.stdout).group(1))
+
+
+sample_bleu_scores = [get_sample_bleu_score()]
 print()
-print('bleu scores')
-print('===========')
-print_statistics(bleu_scores)
-pyplot.figure()
-pyplot.hist(bleu_scores, bins=10, range=(0.5, 1.0))
-pyplot.title('BLEU Score Frequency Histogram')
-pyplot.xlabel('BLEU Score')
-pyplot.ylabel('# of References')
-bleu_score_sample_means = [numpy.mean(bleu_scores)]
+print('coverage = ' + format_statistic(hypothesis.coverage.get_coverage()))
 samples = '{:,}'.format(arguments.samples)
 sample_format_str = '{:>' + str(len(samples)) + ',}'
 precision = str(max(0, len(str(1.0 / arguments.samples)) - 4))
@@ -117,11 +110,11 @@ for sample in range(1, arguments.samples):
         end='',
         file=stderr,
         flush=True)
-    bleu_score_sample_means.append(numpy.mean(get_bleu_score_sample()))
+    sample_bleu_scores.append(get_sample_bleu_score())
 
 print(end='\n\n', file=stderr)
-print('bleu score sample means')
-print('=======================')
-print_statistics(bleu_score_sample_means)
+print('sample BLEU scores')
+print('==================')
+print_statistics(sample_bleu_scores)
 print()
 pyplot.show()
