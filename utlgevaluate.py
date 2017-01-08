8d229fc56eaa977b41eda7eea6d5aa2df48a4a76
matthew <m5w@users.noreply.github.com>
2017-01-08 18:31:46 -0500

    Fixes: write header and footer of sgm files among others

--- utlgevaluate.py
+++ utlgevaluate.py
@@ -14,10 +14,11 @@ import subprocess
 argument_parser = ArgumentParser()
 argument_parser.add_argument('xml')
 argument_parser.add_argument('samples', type=int)
-argument_parser.add_argument('mteval')
-argument_parser.add_argument('ref_file')
-argument_parser.add_argument('src_file')
-argument_parser.add_argument('tst_file')
+argument_parser.add_argument('trglang', type=str)
+argument_parser.add_argument('mteval', type=str)
+argument_parser.add_argument('ref_file', type=str)
+argument_parser.add_argument('src_file', type=str)
+argument_parser.add_argument('tst_file', type=str)
 arguments = argument_parser.parse_args()
 lineariser = Lineariser()
 
@@ -34,6 +35,13 @@ def print_seg(id_, seg, file):
 
 with open(arguments.ref_file, mode='w') as ref_file, \
      open(arguments.src_file, mode='w') as src_file:
+    print(
+        '<refset trglang="' + arguments.trglang + '" setid="1" srclang="any">',
+        file=ref_file)
+    print('<doc sysid="ref" docid="1">', file=ref_file)
+    print('<srcset setid="1" srclang="any">', file=src_file)
+    print('<doc docid="1">', file=src_file)
+
     for id_, sentence in enumerate(treebank, start=1):
         sentence_list = list(sentence.get_wordlines().items())
         sentence_list.sort()
@@ -41,6 +49,11 @@ with open(arguments.ref_file, mode='w') as ref_file, \
         reference_len_list.append(len(reference))
         print_seg(id_, reference, ref_file)
         print_seg(id_, reference, src_file)
+
+    print('</doc>', file=src_file)
+    print('</srcset>', file=src_file)
+    print('</doc>', file=ref_file)
+    print('</refset>', file=ref_file)
 
 
 def format_statistic(statistic):
@@ -73,23 +86,41 @@ pyplot.hist(reference_len_list, bins=10, range=(0, 50))
 pyplot.title('Reference Length Frequency Histogram')
 pyplot.xlabel('Reference Length (# of Words)')
 pyplot.ylabel('# of References')
-BLEU_SCORE = re.compile('BLEU score = ([01]\.\d{4,4})')
+BLEU_SCORE = re.compile(b'BLEU score = ([01]\.\d{4,4})')
 
 
 def get_sample_bleu_score():
     with open(arguments.tst_file, mode='w') as tst_file:
+        print(
+            '<tstset trglang="' + arguments.trglang +
+            '" setid="1" srclang="any">',
+            file=tst_file)
+        print('<doc sysid="1" docid="1">', file=tst_file)
+
         for id_, sentence in enumerate(treebank, start=1):
             sentence.linearise(lineariser, 1, True)
             linearisation = sentence.get_linearisations()[0]
             hypothesis = [word.get_form().lower() for word in linearisation]
             print_seg(id_, hypothesis, tst_file)
 
+        print('</doc>', file=tst_file)
+        print('</tstset>', file=tst_file)
+
     completed_process = subprocess.run([
-        'perl', arguments.mteval, '-r', arguments.ref_file, 's',
-        arguments.src_file, 't', arguments.tst_file
+        'perl', arguments.mteval, '-r', arguments.ref_file, '-s',
+        arguments.src_file, '-t', arguments.tst_file
     ],
                                        stdout=subprocess.PIPE)
-    completed_process.check_returncode()
+
+    try:
+        completed_process.check_returncode()
+    except (subprocess.CalledProcessError):
+        stdout.flush()
+        stderr.flush()
+        print(completed_process.stdout, flush=True)
+        print(completed_process.stderr, file=stderr, flush=True)
+        raise
+
     return float(BLEU_SCORE.search(completed_process.stdout).group(1))
 
 
