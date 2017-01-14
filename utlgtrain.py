fbd3b93144e1f3df129bbc23f31facecff491d42
matthew <m5w@users.noreply.github.com>
2017-01-14 02:26:28 -0500

    Reset dependency_tree when training after projectivization

--- utlgtrain.py
+++ utlgtrain.py
@@ -18,8 +18,14 @@ heads = {}
 nodes = {}
 
 argument_parser = ArgumentParser()
-argument_parser.add_argument('--lemma-file', type=str)
-argument_parser.add_argument('--projectivise', action='store_true')
+argument_parser.add_argument(
+    '--lemma-file',
+    type=str,
+    help='the name of the file to get rule-differentiating lemmas from')
+argument_parser.add_argument(
+    '--projectivise',
+    action='store_true',
+    help='projectivise each sentence before processing it')
 arguments = argument_parser.parse_args()
 
 try:
@@ -29,7 +35,7 @@ except (TypeError):
     lemmas = []
 
 
-def proc_node(h, n, i, r, cnf):  #{
+def proc_node(h, n, i, r, cnf):
     rule = {}
     head = nodes[i]
     lin_h = int(head[0])
@@ -37,87 +43,83 @@ def proc_node(h, n, i, r, cnf):  #{
     deprel_h = head[7]
     rule[lin_h] = (pos_h, deprel_h)
     lem_h = head[2]
+
     if lem_h in lemmas:
         rule[lin_h] += (lem_h, )
-    if i in h:  #{
-        for child in h[i]:  #{
+
+    if i in h:
+        for child in h[i]:
             dep = nodes[child]
             lin_c = int(dep[0])
             pos_c = dep[3]
             deprel_c = dep[7]
             rule[lin_c] = (pos_c, deprel_c)
             lem_c = dep[2]
+
             if lem_c in lemmas:
                 rule[lin_c] += (lem_c, )
-        #}
+
         k = list(rule.keys())
         k.sort()
-        #print('R', (lin_h, pos_h, deprel_h), rule);
         childs = ''
         rhead = ''
         config_head = ''
         config_childs = ''
-        for ord in range(0, len(k)):  #{
-            j = k[ord]
-            #print(' ', j, rule[j], ':', ord);
-            if j == lin_h:  #{
-                rhead = '/'.join([str(ord)] + list(rule[j]))
+
+        for ord_ in range(0, len(k)):
+            j = k[ord_]
+
+            if j == lin_h:
+                rhead = '/'.join([str(ord_)] + list(rule[j]))
                 config_head = '/'.join(rule[j])
-            else:  #{
-                if childs == '':  #{
-                    childs = '/'.join([str(ord)] + list(rule[j]))
+            else:
+                if childs == '':
+                    childs = '/'.join([str(ord_)] + list(rule[j]))
                     config_childs = '/'.join(rule[j])
-                else:  #{
-                    childs = childs + '|' + '/'.join([str(ord)] + list(rule[
+                else:
+                    childs = childs + '|' + '/'.join([str(ord_)] + list(rule[
                         j]))
                     config_childs = config_childs + '|' + '/'.join(rule[j])
-                #}
-                #}
 
-                #}
         rule = rhead + '!' + childs
         config_rule = config_head + '!' + config_childs
 
-        if rule not in r:  #{
+        if rule not in r:
             r[rule] = 0
-        #}
+
         r[rule] += 1
-        if config_rule not in configs:  #{
+
+        if config_rule not in configs:
             configs[config_rule] = {}
-        #}
-        if rule not in configs[config_rule]:  #{
+
+        if rule not in configs[config_rule]:
             configs[config_rule][rule] = 0
-        #}
+
         configs[config_rule][rule] += 1
-        for child in h[i]:  #{
+
+        for child in h[i]:
             res = proc_node(h, n, child, r, cnf)
             r = res[0]
             cnf = res[1]
-        #}
-        #}
+
     return (r, cnf)
 
-
-#}
 
 ### Process a CoNLL-U file
 
 if arguments.projectivise:
     dependency_tree = DependencyTree()
 
-for line in stdin.readlines():  #{
-
-    if line[0] == '#':  #{
+for line in stdin.readlines():
+    if line[0] == '#':
         continue
-    #}
 
     # Add a node to the tree.
-    if line.count('\t'):  #{
+    if line.count('\t'):
         row = line.split('\t')
 
-        if row[0].count('-') > 0:  #{
+        if row[0].count('-') > 0:
             continue
-        #}
 
         if arguments.projectivise:
             dependency_tree.add_node(row)
@@ -125,16 +127,15 @@ for line in stdin.readlines():  #{
             bas = int(row[6])
             cur = int(row[0])
 
-            if bas not in heads:  #{
+            if bas not in heads:
                 heads[bas] = []
-            #}
-            heads[bas].append(cur)
-            if cur not in nodes:  #{
-                nodes[cur] = row
-            #}
-            #}
 
-    if line == '\n':  #{
+            heads[bas].append(cur)
+
+            if cur not in nodes:
+                nodes[cur] = row
+
+    if line == '\n':
         if arguments.projectivise:
             dependency_tree.add_children()
             dependency_tree.calculate_domains()
@@ -150,66 +151,68 @@ for line in stdin.readlines():  #{
                     heads[head] = []
 
                 heads[head].append(id_)
+                nodes[id_] = [
+                    dependency_tree_node.fields['id'],
+                    dependency_tree_node.fields['form'],
+                    dependency_tree_node.fields['lemma'],
+                    dependency_tree_node.fields['upostag'],
+                    dependency_tree_node.fields['xpostag'],
+                    dependency_tree_node.fields['feats'],
+                    dependency_tree_node.fields['head'],
+                    dependency_tree_node.fields['deprel'],
+                    dependency_tree_node.fields['deps'],
+                    dependency_tree_node.fields['misc']
+                ]
 
-                if id_ not in nodes:
-                    nodes[id_] = [
-                        dependency_tree_node.fields['id'],
-                        dependency_tree_node.fields['form'],
-                        dependency_tree_node.fields['lemma'],
-                        dependency_tree_node.fields['upostag'],
-                        dependency_tree_node.fields['xpostag'],
-                        dependency_tree_node.fields['feats'],
-                        dependency_tree_node.fields['head'],
-                        dependency_tree_node.fields['deprel'],
-                        dependency_tree_node.fields['deps'],
-                        dependency_tree_node.fields['misc']
-                    ]
+            dependency_tree = DependencyTree()
 
-        for i in heads[0]:  #{
+        for i in heads[0]:
             (rules, configs) = proc_node(heads, nodes, i, rules, configs)
-        #}
+
         heads = {}
         nodes = {}
-    #}
-    #}
 
-    ### Now we print out the rules we learnt.
+### Now we print out the rules we learnt.
 
 print('<?xml version="1.0"?>')
 print('<linearisation-rules>')
 configs = list(configs.items())
 configs.sort()
-for c, i in configs:  #{
+
+for c, i in configs:
     total = 0
     i = list(i.items())
     i.sort()
-    for r, j in i:  #{
+
+    for r, j in i:
         total = total + j
-    #}
-    for r, j in i:  #{
+
+    for r, j in i:
         prob = float(j) / float(total)
         print('%.2f\t%d\t%d\t%s\t%s' % (prob, total, j, c, r), file=stderr)
         head = r.split('!')[0].split('/')
         deps = r.split('!')[1].split('|')
 
         print('<def-rule p="%.4f">' % (prob))
+
         if len(head) == 4:
-            print('  <NODE ord="%s" si="%s" pos="%s" lem="%s">' %
+            print('  <NODE ord_="%s" si="%s" pos="%s" lem="%s">' %
                   (head[0], head[2], head[1], head[3]))
         else:
-            print('  <NODE ord="%s" si="%s" pos="%s">' %
+            print('  <NODE ord_="%s" si="%s" pos="%s">' %
                   (head[0], head[2], head[1]))
-        for dep in deps:  #{
+
+        for dep in deps:
             deprow = dep.split('/')
+
             if len(deprow) == 4:
-                print('     <NODE ord="%s" si="%s" pos="%s" lem="%s"/>' %
+                print('     <NODE ord_="%s" si="%s" pos="%s" lem="%s"/>' %
                       (deprow[0], deprow[2], deprow[1], deprow[3]))
             else:
-                print('     <NODE ord="%s" si="%s" pos="%s"/>' %
+                print('     <NODE ord_="%s" si="%s" pos="%s"/>' %
                       (deprow[0], deprow[2], deprow[1]))
-        #}
+
         print('  </NODE>')
         print('</def-rule>')
-    #}
-    #}
+
 print('</linearisation-rules>')
